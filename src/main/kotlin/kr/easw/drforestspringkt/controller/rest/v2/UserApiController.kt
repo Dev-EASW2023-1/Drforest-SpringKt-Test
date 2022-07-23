9a3d410bf441c88fb16c420991f7a6c6a08b3fe8
skywolf46 <skywolf461@gmail.com>
2022-07-24 06:45:19 +0900

    feat(API/v2)!: 데이터 벌크 업로드 API 추가

---
+++ src/main/kotlin/kr/easw/drforestspringkt/controller/rest/v2/UserApiController.kt
@@ -0,0 +1,28 @@
+package kr.easw.drforestspringkt.controller.rest.v2
+
+import kr.easw.drforestspringkt.auth.UserAccountData
+import kr.easw.drforestspringkt.model.dto.UserDataUploadRequest
+import kr.easw.drforestspringkt.model.dto.UserDataUploadResponse
+import kr.easw.drforestspringkt.service.*
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v2/api/user")
+class UserApiController(
+    val authenticateService: AuthenticateService,
+    val userDataService: UserActivityDataService,
+) {
+    @PutMapping("/upload")
+    fun onUploadData(
+        @AuthenticationPrincipal user: UserAccountData,
+        @RequestBody dto: List<UserDataUploadRequest>,
+    ): ResponseEntity<UserDataUploadResponse> {
+        userDataService.upload(user, dto)
+        return ResponseEntity.ok(UserDataUploadResponse(true))
+    }
+}
