e525a2125ad446a2708ae329535e3e1934961772
JaeGyeong <fb_olddog@naver.com>
2022-07-28 22:39:26 +0900

    feat: 사용자 지역 이전 서비스, API 추가

--- src/main/kotlin/kr/easw/drforestspringkt/service/RegionManagementService.kt
+++ src/main/kotlin/kr/easw/drforestspringkt/service/RegionManagementService.kt
@@ -1,12 +1,20 @@
 package kr.easw.drforestspringkt.service
 
 import kr.easw.drforestspringkt.model.dto.ListRegionResponse
+import kr.easw.drforestspringkt.model.dto.UpdateRegionToUserResponse
 import kr.easw.drforestspringkt.model.entity.RegionEntity
 import kr.easw.drforestspringkt.model.repository.RegionRepository
+import kr.easw.drforestspringkt.model.repository.UserAccountRepository
+import kr.easw.drforestspringkt.model.repository.UserDataRepository
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Service
 
 @Service
-class RegionManagementService(val regionRepository: RegionRepository) {
+class RegionManagementService(
+    val regionRepository: RegionRepository,
+    val userAccountRepository : UserAccountRepository,
+    val userDataRepository : UserDataRepository
+    ) {
     fun listRegion(): List<RegionEntity> {
         return regionRepository.getAllBy()
     }
@@ -32,4 +40,15 @@ class RegionManagementService(val regionRepository: RegionRepository) {
         return regionRepository.getByRegionName(region).orElseGet { null }
     }
 
+    fun updateRegionToUser(region: String, userId: String): UpdateRegionToUserResponse {
+        val user = userAccountRepository.findByUserId(userId).orElseThrow {
+            BadCredentialsException("해당 유저를 찾을 수 없습니다.")
+        }
+
+        if (getRegion(region) == null)
+            return UpdateRegionToUserResponse(false, "등록되지 않은 지역입니다.")
+
+        userDataRepository.updateRegion(getRegion(region)!!, user)
+        return UpdateRegionToUserResponse(true,"지역 이전에 성공하였습니다.")
+    }
 }
